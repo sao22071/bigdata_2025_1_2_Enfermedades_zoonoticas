06ccf615794d0e055705c2f0f7ca8f6e756077a8
sao22071 <56183094+sao22071@users.noreply.github.com>
2025-05-11 06:17:10 +0000

    base de datos, main.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="edu_bidata",
+    author="Santiago Alvarez Olivera",
+    author_email="santiago.alvarez@est.iudigital.edu.co",
+    description="",
+    install_requires=[
+        "pandas",
+        "requests",
+    ] 
+)
